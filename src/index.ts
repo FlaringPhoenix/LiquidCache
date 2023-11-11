57c4e1f7bfe013735b6b05e194c2ccc441e3adf6
Brendan Jennings <bre@sdf.org>
2023-11-11 23:51:01 +0000

    📝 Add TS JSdoc to LiquidCache

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,27 @@
+/**
+ * @abstract LiquidCache is an easy-to-setup map-based caching system for Node.js. It allows you to store key-value pairs in memory and configure cache expiration intervals.
+ * @author FlaringPhoenix
+ * @template K - Type of key
+ * @template V - Type of value
+ */
 class LiquidCache<K, V> extends Map<K, V> {
     private expirationMap = new Map<K, NodeJS.Timeout>();
   
+    /**
+     * @constructs LiquidCache
+     * @param defaultExpiration Default expiration time in milliseconds (Default: 60000)
+     */
     constructor(private defaultExpiration: number = 60000) {
       super();
     }
   
+    /**
+     * Sets a key-value pair in the cache
+     * @param key Key to set
+     * @param value Value to be stored within key
+     * @param expiration (Optional) Expiration time in milliseconds (Default: 60000)
+     * @returns The cache instance
+     */
     set(key: K, value: V, expiration?: number): this {
       // If the key already exists, clear the previous timeout
       if (this.expirationMap.has(key)) {
@@ -20,6 +37,11 @@ class LiquidCache<K, V> extends Map<K, V> {
       return this;
     }
   
+    /**
+     * Pop's an element from the cache
+     * @param key Key to get
+     * @returns The value stored within the key
+     */
     delete(key: K): boolean {
       const timeout = this.expirationMap.get(key);
       if (timeout) {
@@ -30,7 +52,9 @@ class LiquidCache<K, V> extends Map<K, V> {
       return super.delete(key);
     }
   
-    // Optionally, you can clear the entire cache and all timeouts
+    /**
+     * Clears the entire cache and all timeouts
+     */
     clear(): void {
       for (const timeout of this.expirationMap.values()) {
         clearTimeout(timeout);
